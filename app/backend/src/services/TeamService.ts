9fd8777ebcc3275fe8815309f54a5d2e8c678587
PauloBrandino <contatobrandino@gmail.com>
2023-06-21 14:16:09 -0300

    cria camada service

---
+++ app/backend/src/services/TeamService.ts
@@ -0,0 +1,14 @@
+import ITeamModel from "../Interfaces/ITeamModel";
+import ITeams from "../Interfaces/ITeams";
+import { ServiceResponse } from "../Interfaces/ServiceResponse";
+import TeamModel from "../models/TeamModel";
+
+export default class TeamService {
+    
+    constructor(private teamModel: ITeamModel = new TeamModel()){}
+    public async getAllTeams(): Promise<ServiceResponse<ITeams[]>> {
+        const allTeams = await this.teamModel.findAll()
+
+        return { status: 'SUCCESS', data: allTeams}
+    }
+}
