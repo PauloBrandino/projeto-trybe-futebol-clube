bea06c9d3a05606f21eb28249f4d62b5714f1ed3
PauloBrandino <contatobrandino@gmail.com>
2023-06-21 12:45:36 -0300

    criado TeamModel e feito correção do dockerfile

--- app/backend/src/Interfaces/ITeams.ts
+++ app/backend/src/Interfaces/ITeams.ts
@@ -1,4 +1,4 @@
 export default interface ITeams {
     id: number,
-    team_name: string
+    teams_name: string
 }
---
+++ app/backend/src/models/TeamModel.ts
@@ -0,0 +1,16 @@
+import ITeams from "../Interfaces/ITeams";
+import ITeamModel from "../Interfaces/ITeamModel";
+import SequelizeTeam from "../database/models/SequelizeTeam";
+
+export default class TeamModel implements ITeamModel {
+    private _model = SequelizeTeam;
+
+    async findAll(): Promise<ITeams[]> {
+        const dbData = await this._model.findAll();
+
+        return dbData.map(({ id, teams_name}) => (
+            { id, teams_name }
+        ));
+    }
+
+}
