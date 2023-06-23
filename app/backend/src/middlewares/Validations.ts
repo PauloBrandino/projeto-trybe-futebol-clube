f2274fde99e279216ec334174aa4e3732eb3672c
PauloBrandino <contatobrandino@gmail.com>
2023-06-23 14:31:04 -0300

    fix lint

--- app/backend/src/middlewares/Validations.ts
+++ app/backend/src/middlewares/Validations.ts
@@ -5,7 +5,7 @@ import { verifyToken } from '../utils/JWTUtils';
 export default class Validations {
   static validateLogin(req: Request, res: Response, next: NextFunction): Response | void {
     const user = req.body;
-    
+
     if (!user.email || !user.password) {
       return res.status(400).json({ message: 'All fields must be filled' });
     }
