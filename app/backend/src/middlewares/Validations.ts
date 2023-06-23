6a121c012079311c9e9fc8abf2cdbfdb4896db08
PauloBrandino <contatobrandino@gmail.com>
2023-06-23 14:12:44 -0300

    refatora validations e statusHTTP

--- app/backend/src/middlewares/Validations.ts
+++ app/backend/src/middlewares/Validations.ts
@@ -5,10 +5,8 @@ import { verifyToken } from '../utils/JWTUtils';
 export default class Validations {
   static validateLogin(req: Request, res: Response, next: NextFunction): Response | void {
     const user = req.body;
-    const requiredFields = ['email', 'password'];
-    const notFoundField = requiredFields.find((field) => !(field in user));
-
-    if (notFoundField) {
+    
+    if (!user.email || !user.password) {
       return res.status(400).json({ message: 'All fields must be filled' });
     }
 
