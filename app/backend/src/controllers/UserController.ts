8689d5f47b62d97a9cec7147fa256e81c12f7ef5
PauloBrandino <contatobrandino@gmail.com>
2023-06-23 12:53:15 -0300

    cria rota e controller para /login/role

--- app/backend/src/controllers/UserController.ts
+++ app/backend/src/controllers/UserController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import UserService from '../services/UserService';
 import mapStatusHTTP from '../utils/mapStatusHTTP';
+import { verifyToken } from '../utils/JWTUtils';
 
 export default class UserController {
   constructor(
@@ -17,5 +18,17 @@ export default class UserController {
       return res.status(code).json(serviceResponse.data);
     }
     return res.status(200).json(serviceResponse.data);
+  }
+
+  public async userRole(req: Request, res: Response): Promise<Response> {
+    const { authorization } = req.headers;
+
+    const verify = verifyToken.bind(this)(authorization as string);
+
+    if (typeof verify === 'string') {
+      return res.status(401).json({ message: 'Token must be a valid token' });
+    }
+
+    return res.status(201).json({ role: verify.role });
   }
 }
